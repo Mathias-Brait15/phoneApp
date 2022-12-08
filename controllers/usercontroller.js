dd020f5ead776b355d3e98d4541d4b18e0af15eb
Mathias-Brait15 <61750659+Mathias-Brait15@users.noreply.github.com>
2022-12-08 10:10:41 +0700

    initial commit

---
+++ controllers/usercontroller.js
@@ -0,0 +1,62 @@
+const {User} = require('../models/index');
+const { Op } = require("sequelize");
+const bcrypt = require('bcryptjs');
+
+class Controller{
+
+    static renderRegister(request , response){
+       const error = request.query.error
+       response.render('formRegister' , {error})
+    }
+
+    static handleRegister(request , response){
+       const {email , password , role} = request.body 
+       User.create({email , password , role})
+       .then((newUser) => {
+        response.redirect('/login')
+       }).catch((err) => {
+        let errorMassage = []
+        if(err.name === 'SequelizeValidationError'){
+            err.errors.forEach((el) => {
+                errorMassage.push(el.message)
+            })
+            response.redirect(`/register?error=${errorMassage}`)
+        }else{
+            response.send(err)
+        }
+       });
+    }
+
+    static renderLogin(request , response){
+        const errorMessage = request.query.error
+        response.render('loginForm' , {errorMessage} )
+    }
+
+    static handleLogin(request , response){
+        const {email , password} = request.body
+        
+        User.findOne({where : {
+            email : {
+                [Op.iLike] : `%${email}%` 
+            }
+        }})
+        .then((user) => {
+            if(user){
+                const isValidate = bcrypt.compareSync(password , user.password)
+                if(isValidate){
+                    response.send('benar')
+                }else{
+                    const errorMessage = 'Invalid Username / Password' 
+                    response.redirect(`/login?error=${errorMessage}`)
+                }
+            }else{
+                const errorMessage = "Username / Password not registered yet"
+                return response.redirect(`/login?error=${errorMessage}`)
+            }
+        }).catch((err) => {
+           response.send(err)
+        });
+    }
+}
+
+module.exports = Controller
---
+++ migrations/20221207183602-create-profile.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+   up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullName: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      amount: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+   down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Profiles');
+  }
+};
